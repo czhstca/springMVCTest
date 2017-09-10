b4435a4e60c49d250242ca9139bcdb6c93385dbc
czhstca <496427931@qq.com>
2017-09-10 18:19:14 +0800

    SpringMVC入门程序

---
+++ springMVC_day01/src/com/pingan/ssm/controller/ItemController2.java
@@ -0,0 +1,54 @@
+package com.pingan.ssm.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.pingan.ssm.pojo.Items;
+
+public class ItemController2 implements HttpRequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		//调用service查找 数据库，查询商品列表，这里使用静态数据模拟
+		List<Items> itemsList = new ArrayList<Items>();
+		//向list中填充静态数据
+		
+		Items items_1 = new Items(); 
+		items_1.setName("联想笔记本");
+		items_1.setPrice(6000f);
+		items_1.setDetail("ThinkPad T430 联想笔记本电脑！");
+		
+		Items items_2 = new Items();
+		items_2.setName("苹果手机"); 
+		items_2.setPrice(5000f);
+		items_2.setDetail("iphone6苹果手机！");
+		
+		itemsList.add(items_1);
+		itemsList.add(items_2);
+
+		arg0.setAttribute("itemsList",itemsList );
+		
+		//设置转发的视图
+		arg0.getRequestDispatcher("/WEB-INF/jsp/items/itemsList.jsp").forward(arg0, arg1);
+		
+		//一般使用这种方式可以response中响应json字符串
+		/*
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json;charset=utf-8");
+		response.getWriter().write("json串");*/
+
+		
+	}
+
+
+
+}
